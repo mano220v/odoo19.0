096f6402b2868c92e3c637406b358b7701f93fe0
manoijv_220 <manoijv_220@gftdsk1331>
2026-07-01 10:35:34 +0530

    woo

---
+++ ow_woocommerce_connector/__manifest__.py
@@ -0,0 +1,42 @@
+{
+    "name": "WooCommerce Connector",
+    "version": "19.0.1.0.0",
+    "category": "Sales/eCommerce",
+    "summary": "Connect WooCommerce with Odoo products, customers, and orders",
+    "description": """
+WooCommerce Connector
+=====================
+
+Synchronize WooCommerce products, customers, and orders with Odoo. The module
+includes multi-store configuration, import and export actions, mapping records,
+sync logs, and a backend dashboard for operational visibility.
+    """,
+    "author": "Odoo Wings",
+    "website": "https://apps.odoo.com/apps/modules/browse?author=Odoo%20Wings",
+    "support": "vsmanoj144@gmail.com",
+    "license": "OPL-1",
+    'price': 85.00,
+    "depends": ["base", "sale_management", "stock", "web"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/woo_mapping_views.xml",
+        "views/woo_sync_log_views.xml",
+        "views/woo_instance_views.xml",
+        "views/woo_dashboard_views.xml",
+        "views/woo_menus.xml",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "ow_woocommerce_connector/static/src/js/woo_dashboard.js",
+            "ow_woocommerce_connector/static/src/xml/woo_dashboard.xml",
+            "ow_woocommerce_connector/static/src/scss/woo_dashboard.scss",
+        ],
+    },
+    "images": [
+        "static/description/banner.png",
+        "static/description/icon.png",
+    ],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
